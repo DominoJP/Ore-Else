94e82b49a8978e8fee9489567e31c578638064c2
SeveredNode <114021468+SeveredNode@users.noreply.github.com>
2022-11-22 12:41:50 -0800

    added rough movement to indicator

---
+++ Ore Else/Assets/HammerIndicatorController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HammerIndicatorController : MonoBehaviour
+{
+
+    public HammerMinigame hammerGameScript;
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        hammerGameScript = GameObject.Find("AnvilSurface").GetComponent<HammerMinigame>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("TopBumper") && hammerGameScript.isMovingUp)
+        {
+            hammerGameScript.isMovingUp = false;
+            hammerGameScript.isMovingDown = true;
+            hammerGameScript.moveDirection = new Vector2(0, -1);
+        }
+
+        if (other.CompareTag("BottomBumper") && hammerGameScript.isMovingDown)
+        {
+            hammerGameScript.isMovingUp = true;
+            hammerGameScript.isMovingDown = false;
+            hammerGameScript.moveDirection = new Vector2(0, 1);
+        }
+
+    }
+
+
+}
--- Ore Else/Assets/Scripts/HammerMinigame.cs
+++ Ore Else/Assets/Scripts/HammerMinigame.cs
@@ -5,26 +5,49 @@ using UnityEngine;
 public class HammerMinigame : MonoBehaviour
 {
 
-    public PlayerCollisionManager playerColScript;
+    
     public float currentWeaponScore;
     public float distFromTarget;
     public GameObject indicator;
     public GameObject target;
     public GameObject fillBar;
+    public Rigidbody2D rbIndicator;
 
+    public float moveSpeed;
+    public Vector2 moveDirection;
+    public bool isMovingUp;
+    public bool isMovingDown;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        playerColScript = GameObject.Find("Player").GetComponent<PlayerCollisionManager>();
+        moveDirection = new Vector2(0, 1);
+        isMovingDown = false;
+        isMovingUp = true;
         fillBar = GameObject.Find("FillBar");
         target = GameObject.Find("Target");
         indicator = GameObject.Find("Indicator");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageIndicatorMovement();
     }
+
+
+    public void ManageIndicatorMovement()
+    {
+        rbIndicator.AddForce(moveDirection * moveSpeed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+       
+    }
+
+
 }
