b21440c44af99b4c73d6b21a086c0e64d8750a2e
ELicenciado <drpastranaga@hotmail.com>
2022-11-29 04:12:59 -0800

    Player Has Animations
    Player changes animations and back to idle depending on position

--- Ore Else/Assets/Scripts/PlayerMovement.cs
+++ Ore Else/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
     Vector2 moveDirection;
+    public Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -21,6 +23,11 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection.x = Input.GetAxis("Horizontal");
         moveDirection.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vorizontal", moveDirection.y);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+
     }
 
     private void FixedUpdate()
